f0fa783fcd3c6919005902c76b26c3fceaf74524
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-05-01 15:11:19 +0900

    summaryだけ取得

--- web-calendar/react-google-calendar/src/App.js
+++ web-calendar/react-google-calendar/src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'; // useStateをインポート
+
 // 1.パッケージを読み込む
 import ApiCalendar from "react-google-calendar-api";
 import './App.css';
@@ -7,42 +9,50 @@ const apiCalendar = new ApiCalendar(config);
 const calendarId = "9b217b7c39e5a52468774ce79daa0703f78ad7928c8bae104cdaa811be68618e@group.calendar.google.com";
 
 function App() {
-  const items = [];
+  // 2. itemsをuseStateフックで状態管理する
+  const [items, setItems] = useState([]);
 
   const getEvents = async () => {
     return new Promise(async (resolve, reject) => {
-      // 2.認証チェック
+      // 3.認証チェック
       if (apiCalendar.sign) {
-         // 3.イベントの取得
-         apiCalendar.listEvents({
-            // 先月の予定を取得
-            timeMin: new Date(new Date().getFullYear(), new Date().getMonth() - 1).toISOString(),
-            timeMax: new Date().toISOString(),
-            showDeleted: true,
-            maxResults: 10,
-            orderBy: 'updated'
-          },calendarId).then(({ result }) => {
-            if (result.items) {
-              console.log("Events From Calendar", result.items);
-              items.push(...result.items);
-            } else {
-              console.log("No Events")
-            }
+        // 4.イベントの取得
+        apiCalendar.listEvents({
+          // 先月の予定を取得
+          timeMin: new Date(new Date().getFullYear(), new Date().getMonth() - 1).toISOString(),
+          timeMax: new Date().toISOString(),
+          showDeleted: true,
+          maxResults: 10,
+          orderBy: 'updated'
+        }, calendarId).then(({ result }) => {
+          if (result.items) {
+            console.log("Events From Calendar", result.items);
+            // 5. itemsを更新する
+            setItems(result.items);
+          } else {
+            console.log("No Events")
+          }
 
-            resolve(result);
-          });
+          resolve(result);
+        });
       } else {
-        // 2.認証していなければOAuth認証
+        // 6.認証していなければOAuth認証
         await apiCalendar.handleAuthClick();
         reject("Sign in first");
-      
+
       }
     })
   };
 
   return (
     <div className="App">
+      {/* 7. itemsをマップして表示 */}
       <button onClick={getEvents}>Get Events</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item.summary}</li>
+        ))}
+      </ul>
     </div>
   );
 }
