7d0b0894e880a8f358d3e982c834e89a8f03baa5
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-05-01 14:11:27 +0900

    カレンダーの情報を取得できるようにする

--- web-calendar/react-google-calendar/src/App.js
+++ web-calendar/react-google-calendar/src/App.js
@@ -7,20 +7,24 @@ const apiCalendar = new ApiCalendar(config);
 const calendarId = "9b217b7c39e5a52468774ce79daa0703f78ad7928c8bae104cdaa811be68618e@group.calendar.google.com";
 
 function App() {
+  const items = [];
+
   const getEvents = async () => {
     return new Promise(async (resolve, reject) => {
       // 2.認証チェック
       if (apiCalendar.sign) {
          // 3.イベントの取得
          apiCalendar.listEvents({
-            timeMin: new Date().toISOString(),
-            timeMax: new Date(new Date() + 864000).toISOString(),
+            // 先月の予定を取得
+            timeMin: new Date(new Date().getFullYear(), new Date().getMonth() - 1).toISOString(),
+            timeMax: new Date().toISOString(),
             showDeleted: true,
             maxResults: 10,
             orderBy: 'updated'
           },calendarId).then(({ result }) => {
             if (result.items) {
               console.log("Events From Calendar", result.items);
+              items.push(...result.items);
             } else {
               console.log("No Events")
             }
